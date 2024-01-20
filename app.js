7d5392e25f85d8be1f254dce804c819a3fb6133a
Nehemiah <nehemiahcheruiyot7069@gmail.com>
2024-01-20 21:45:38 +0300

    Get the meal details content element

--- app.js
+++ app.js
@@ -4,3 +4,5 @@ const searchBtn = document.getElementById('search-btn');
 // Get the meal list element
 const mealList = document.getElementById('meal');
 
+// Get the meal details content element
+const mealDetailsContent = document.querySelector('.meal-details-content');
